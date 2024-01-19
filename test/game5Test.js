a6fce8d4fb7dd9a33566d9bf1cefa6b5a8c5dcd4
cgrade <gradeisageek@gmail.com>
2024-01-19 16:47:10 +0100

    Solution complete

--- test/game5Test.js
+++ test/game5Test.js
@@ -5,15 +5,15 @@ describe('Game5', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
+    const signer = ethers.provider.getSigner(0);
 
-    return { game };
+    return { game, signer };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
-
+    const { game, signer } = await loadFixture(deployContractAndSetVariables);
+    address = bytes20(signer.getAddress());
     // good luck
-
-    await game.win();
+    await game.connect(address).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
