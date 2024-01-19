f177a7d6e676f511ddc1319b66d7867db0cdc13b
cgrade <gradeisageek@gmail.com>
2024-01-19 15:50:01 +0100

    solution to Asignment Quizz

--- test/game4Test.js
+++ test/game4Test.js
@@ -5,15 +5,16 @@ describe('Game4', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
+    const signer1 = ethers.provider.getSigner(0);
 
-    return { game };
+    return { game, signer1  };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, signer1 } = await loadFixture(deployContractAndSetVariables);
 
+    await game.write(signer1.getAddress())
     // nested mappings are rough :}
-
-    await game.win();
+    await game.win(signer1.getAddress());
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
